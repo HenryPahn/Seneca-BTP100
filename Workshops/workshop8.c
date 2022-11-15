7e9de0ba4700e27c689e7dc69a9a4b46de7a57f6
lntv0407 <thoang0303@gmail.com>
2022-11-15 06:19:13 -0500

    body-4 updated

--- Workshops/workshop8.c
+++ Workshops/workshop8.c
@@ -2,40 +2,44 @@
 #include <stdio.h>
 #define MAX_NAME 32
 
-//Define your struct here
+// Define your struct here
 struct student
 {
-    int id; 
+    int id;
     char name[MAX_NAME];
     float GPA;
 };
 
-
-//Define save_student here
-void get_name(char *name){
-    scanf("%[^\n]%*c", name); 
+// Define save_student here
+void get_name(char *name)
+{
+    scanf("%[^\n]%*c", name);
 }
 
-void save_student(struct student *temp) {
+void save_student(struct student *temp)
+{
     printf("Enter student's id.\n");
     scanf("%i", &temp->id);
-    while((getchar()) != '\n');
+    while ((getchar()) != '\n')
+        ;
     printf("Enter student's name.\n");
     get_name(temp->name);
     printf("Enter student's gpa.\n");
     scanf("%f", &temp->GPA);
 }
 
-//Define Print_student here
-void print_student(struct student temp) {
+// Define Print_student here
+void print_student(struct student temp)
+{
     printf("Student's id: %i\n", temp.id);
     printf("Student's name: %s\n", temp.name);
     printf("Student's gpa: %.2f\n", temp.GPA);
 }
 
-int main() {
-    
-    struct student individual; 
+int main()
+{
+
+    struct student individual;
 
     save_student(&individual);
     print_student(individual);
